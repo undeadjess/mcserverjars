d70e677c5885d31f4ae1649b9e5d144a105f7ba3
Jess <68990356+undeadjess@users.noreply.github.com>
2024-08-27 12:24:27 +1200

    Add function to get Minecraft Versions

--- fetcher/index.js
+++ fetcher/index.js
@@ -1,1 +1,46 @@
 
+// getMinecraftVersions
+function getMinecraftVersions() {
+    return new Promise((resolve, reject) => {
+        var mcversions = [];
+        console.log('getVersions running:');
+        url = 'https://launchermeta.mojang.com/mc/game/version_manifest.json';
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                versions = data.versions;
+                versions.forEach(version => {
+                    if (version.type === 'release') {
+                        console.log('got version ', version.id);
+                        mcversions.push(version.id.toString());
+                    }
+                });
+                resolve(mcversions); // Resolve the promise with the results
+            })
+            .catch(error => {
+                console.log('error:', error);
+                resolve(mcversions); // Resolve the promise even if there's an error
+            });
+    });
+}
+
+// Call getVersions and handle the promise
+getMinecraftVersions().then(versions => {
+    console.log("versions:", versions);
+});
+
+
+mcversions = getMinecraftVersions();
+
+// getting scripts
+
+// vanilla
+
+function getVanillaServerURL(version) {
+
+}
+
+
+
+
