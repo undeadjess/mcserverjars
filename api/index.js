88c0e1c6543a7d2f758d3b3b9c61266eb9431019
Jess <68990356+undeadjess@users.noreply.github.com>
2024-08-29 20:42:52 +1200

    add (not working) function to fetch from database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 // api!!
 // still dont konw what im doing
 
+databasePath = '../fetcher/servers.db';
+validServers = ['vanilla', 'paper', 'purpur', 'spigot', 'bukkit', 'forge'];
+port = 8080;
+
+
 
 // api docs (yes im putting them in the code)
 // /api                                     // returns some usage info
@@ -12,17 +17,19 @@
 
 const { json } = require('body-parser');
 const express = require('express');
-
 const app = express();
-const port = 8080;
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(databasePath);
+
+
+// root
 app.get('/', (req, res) => {
     console.log('new request to /');
     res.send('serverjars api');
 });
 
 // actual api stuff
-// with build specified
 app.get('/api/servers/:server/:version/:build', (req, res) => {
     console.log('\nNew request to', req.path);
     const { server, version, build } = req.params;
@@ -33,15 +40,57 @@ app.get('/api/servers/:server/:version/:build', (req, res) => {
 app.get('/api/servers/:server/:version', (req, res) => {
     console.log('\nNew request to', req.path);
     const { server, version } = req.params;
-    res.json(getServerURLs(server, version, null));
+    res.json(getServerURL(server, version, null));
 });
 
 
-function getServerURLs(server, version, build) {
+// get server URLs
+function getServerURL(server, version, build) {
+    // make sure valuse are valid to prevent sql injection
+    // server
+    if (!validServers.includes(server)) {
+        return { error: 'invalid server' };
+    }
+    // version
+    if (!version.match(/\d+\.\d+(\.\d+)?/)) {
+        return { error: 'invalid version' };
+    }
+    // build
+    if (build && !build.match(/\d+/)) {
+        return { error: 'invalid build' };
+    }
+
+
+    console.log('getting server urls for', server, version, build);
+    let query;
+    let params;
+    let download_url;
+    if (build) {
+        query = `SELECT download_url FROM ${server} WHERE version = ? AND build = ?`;
+        params = [version, build];
+    } else {
+        query = `SELECT download_url FROM ${server} WHERE version = ?`;
+        params = [version];
+    }
+    db.get(query, params, (err, row) => {
+        if (err) {
+            console.log('error getting server urls:', err);
+            reject(err);
+        } else {
+            console.log('got server urls:', row);
+            download_url = row;                                // PROBLEMMMM HERE (row is undefined cause its not defined yet because its async)
+        }
+    });
+
+
+
+
+    console.log(download_url);
     return {
         server: server,
         version: version,
         build: build,
+        downloadURL: download_url
     };
 }
 
