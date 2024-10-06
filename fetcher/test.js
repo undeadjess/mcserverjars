65876d9c5e6c5ee8f13e59d6366885f58d31cfbe
Jess <68990356+undeadjess@users.noreply.github.com>
2024-10-06 18:57:50 +1300

    Finish fabric fetcher

--- fetcher/test.js
+++ fetcher/test.js
@@ -111,23 +111,136 @@ async function getPurpurServerURLs() {
 }
 
 // spigot
+// Potential DMCA issues exist - see https://github.com/github/dmca/blob/master/2014/2014-09-05-CraftBukkit.md
+// Complicated - need to build the server jar
 async function getSpigotServerURLs() {
     return null
 }
 
 // bukkit
+// Potential DMCA issues exist - see https://github.com/github/dmca/blob/master/2014/2014-09-05-CraftBukkit.md
+// Complicated - need to build the server jar
 async function getBukkitServerURLs() {
     return null
 }
 
 // forge
+// Complicated - need to build the server jar
 async function getForgeServerURLs() {
     return null
 }
 
 
+// fabric
+async function getFabricServerURLs() {
+    // https://meta.fabricmc.net/v2/versions/loader/1.21.1/0.16.5/1.0.1/server/jar
+    // https://meta.fabricmc.net/v2/versions/loader/<Minecraft Version>/<Fabric Loader Version>/<Installer Version>/server/jar
+
+    const fabricServerURLs = [];
+    const fabricURL = "https://meta.fabricmc.net/v2/versions/loader";
+    console.log('[getFabricServerURLs] Fetching Fabric Server URLs');
+
+    // we will always want the latest installer version...
+    async function getLatestInstallerVersion() {
+        // https://meta.fabricmc.net/v2/versions/installer
+        const installerURL = "https://meta.fabricmc.net/v2/versions/installer";
+        try {
+            const response = await fetch(installerURL);
+            const data = await response.json();
+            return data[0].version; // Return the latest installer version
+        } catch (error) {
+            console.error('[getFabricServerURLs] Error fetching installer version:', error);
+            return null; // Return null if there's an error
+        }
+    }
+
+    // need to get the game versions supported by fabric
+    async function getSupportedStableMinecraftVersions() {
+        // https://meta.fabricmc.net/v2/versions/game
+        const gameURL = "https://meta.fabricmc.net/v2/versions/game";
+        const supportedMinecraftVersions = []
+        try {
+            const response = await fetch(gameURL);
+            const data = await response.json();
+            data.map(version => {
+                // check that stable is true
+                if (version.stable) {
+                    supportedMinecraftVersions.push(version.version);
+                }
+
+            });
+        } catch (error) {
+            console.error('[getFabricServerURLs] Error fetching supported Minecraft versions:', error);
+            return null; // Return null if there's an error
+        }
+        return supportedMinecraftVersions
+    }
+
+    // get fabric loader versions
+    async function getFabricLoaderVersions() {
+        // https://meta.fabricmc.net/v2/versions/loader
+        const loaderURL = "https://meta.fabricmc.net/v2/versions/loader";
+        const fabricLoaderVersions = []
+
+        try {
+            const response = await fetch(loaderURL);
+            const data = await response.json();
+            data.map(version => {
+                fabricLoaderVersions.push(version.version);
+            });
+        } catch (error) {
+            console.error('[getFabricServerURLs] Error fetching Fabric Loader versions:', error);
+            return null; // Return null if there's an error
+        }
+        return fabricLoaderVersions.sort();
+    }
+
+
+    try {
+        const installerVersion = await getLatestInstallerVersion();
+        const supportedMinecraftVersions = await getSupportedStableMinecraftVersions();
+        const fabricLoaderVersions = await getFabricLoaderVersions();
+
+        // for each supported version, get the server jar
+        for (const minecraftVersion of supportedMinecraftVersions) {
+            // data structure: {version: "1.16.4", builds: [{build: 1, fabricLoaderVersion: "something", downloadURL: "something"}]}
+            const fabricServerURLsForVersion = [];
+            let buildCounter = 1;
+            for (const fabricLoaderVersion of fabricLoaderVersions) {
+                const downloadURL = (`https://meta.fabricmc.net/v2/versions/loader/${minecraftVersion}/${fabricLoaderVersion}/${installerVersion}/server/jar`);
+
+                fabricServerURLsForVersion.push({
+                    build: buildCounter++,
+                    fabricLoaderVersion: fabricLoaderVersion,
+                    downloadURL: downloadURL
+                });
+                
+            }
+            fabricServerURLs.push({
+                version: minecraftVersion,
+                builds: fabricServerURLsForVersion
+
+            });
+        }
+
+        console.log('[getFabricServerURLs] Finished Fetching Fabric Server URLs');
+    } catch (error) {
+        console.error('[getFabricServerURLs] Error: ', error);
+    }
+
+    return fabricServerURLs;
+
+};
+
 async function getServerURLs() {
-    func1 = await getPaperServerURLs();
+    func1 = await getFabricServerURLs();
+    // save full list of server urls to log file
+    const fs = require('fs');
+    fs.writeFile('serverurls.log', JSON.stringify(func1), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
     console.log(func1);
 }
 
